c10c68f7a6354d381aee2ad38228fa9dc03061f7
wuchuang127 <812625835@qq.com>
2015-08-13 18:33:37 +0800

    Initial commit

---
+++ app/src/main/java/cn/chuangblog/dragview/MainActivity.java
@@ -0,0 +1,62 @@
+package cn.chuangblog.dragview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity implements View.OnTouchListener {
+
+    private LinearLayout mRrootLayout;
+    private int _xDelta;
+    private int _yDelta;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRrootLayout = (LinearLayout) findViewById(R.id.layout);
+        mRrootLayout.setOnTouchListener(this);
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                _xDelta = X - lParams.leftMargin;
+                _yDelta = Y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                        .getLayoutParams();
+
+                Log.e("TAG",layoutParams.leftMargin+":"+layoutParams.topMargin);
+
+                layoutParams.leftMargin = X - _xDelta;
+                layoutParams.topMargin = Y - _yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+
+                 if(layoutParams.leftMargin<0)
+                     layoutParams.leftMargin = 0;
+
+                view.setLayoutParams(layoutParams);
+                break;
+        }
+        mRrootLayout.invalidate();
+        return true;
+    }
+}
